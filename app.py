2e149f8af4d35367d17b840b3e9e203bf650ff4f
Mitchell Avery <averymr@colorado.edu>
2025-03-24 18:02:52 -0500

    added psycopg2 req and db url to routes

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Mitch A in 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://postgresql_lab10_zj3u_user:fxTXSGOWrx9udgkmfKkmKv58JSQbtMl2@dpg-cvgu7ftumphs73cvu9eg-a/postgresql_lab10_zj3u")
+    conn.close()
+    return "Database connection successful"
